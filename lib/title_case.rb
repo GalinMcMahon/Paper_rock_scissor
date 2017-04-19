1466e842057789bc5e631cff2557c4edf48939ad
Galin McMahon and Mohamed Warsame <mohamed@>
2017-04-19 16:53:32 -0700

    create if statements

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,16 +1,25 @@
 class String
-  define_method(:title_case) do
-    titlecased_words = []
-    no_capital_words = ["to","the","of","from","and","but","or","by","on"]
-    words = self.split()
-    words.each do |word|
+  define_method(:paper) do
+    user_input = self.upcase()
+    array_winner = []
+    winner = {
+      1 => ["AK", "KA", "SL", "LS", "DJ", "JD"],
+      2 => ["AL", "LA", "SJ", "JS", "DK", "KD"],
+      3 => ["AJ", "JA", "SK", "KS", "DL", "LD"]
+    }
+    # words = self.split()
+    user_input.each do |word|
         # first section below is redundant but leaving in just to show
-      if (word != words.at(0)) && (no_capital_words.include?(word))
-        titlecased_words.push(word)
+      if(winner).include?(1)
+        array_winner.push("Player one wins")
+      elsif(winner).include?(2)
+        array_winner.push("Player two wins")
+      elsif(winner).include?(3)
+        array_winner.push("Tie")
       else
-        titlecased_words.push(word.capitalize)
+        array_winner.push("Use valid key")
       end
     end
-    titlecased_words.join(" ")
+    array_winner.push()
   end
 end
